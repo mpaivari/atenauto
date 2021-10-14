45eff68a98a7fb31208998f7b1aaf3013a608481
mpaivari <90454812+mpaivari@users.noreply.github.com>
2021-10-14 23:03:36 +0300

    Update varaus.js

--- varaus.js
+++ varaus.js
@@ -37,8 +37,8 @@ function teeVaraus() {
         break;
     }
    
-   if (rekisteri.value != '' and etunimi.value != '' and sukunimi.value != '' and
-      osoite.value != '' and puhelin.value != '' and txt.value != '')
+   if (rekisteri.value != '' && etunimi.value != '' && sukunimi.value != '' &&
+      osoite.value != '' && puhelin.value != '' && txt.value != '')
    {
      alert('Sinulle on varattu huolto autolle ['+rekisteri.value+'] ensi viikon ' +paiva+' kello '+huoltoKlo+'. Tervetuloa!'); 
    }
